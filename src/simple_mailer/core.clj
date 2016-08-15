c3052a7b464a769b12faf28b79875ce8315b03f2
TashaGospel <nhanhnhanh17399@gmail.com>
2016-08-15 22:21:04 +1200

    First commit

---
+++ src/simple_mailer/core.clj
@@ -0,0 +1,7 @@
+(ns simple-mailer.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println (first args)))
---
+++ test/simple_mailer/core_test.clj
@@ -0,0 +1,7 @@
+(ns simple-mailer.core-test
+  (:require [clojure.test :refer :all]
+            [simple-mailer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
