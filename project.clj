1dc41b745bf2fd2616e93b5fc6aa042d8e279467
TashaGospel <nhanhnhanh17399@gmail.com>
2016-08-18 21:41:06 +1200

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
-(defproject simple-mailer "0.1.0-SNAPSHOT"
+(defproject simple-mailer "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.draines/postal "2.0.0"]
+                 [org.clojure/core.async "0.2.385"]]
   :main ^:skip-aot simple-mailer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
--- src/simple_mailer/core.clj
+++ src/simple_mailer/core.clj
@@ -1,7 +1,50 @@
 (ns simple-mailer.core
+  (:require [postal.core :as postal]
+            [clojure.core.async :as a])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println (first args)))
+(defn get-details []
+  {:username (do (print "Username: ") (flush)
+                 (read-line))
+   :password (do (print "Password: ") (flush)
+                 (if-let [console (System/console)]
+                   (apply str (seq (.readPassword console)))
+                   (read-line)))
+   :to       (do (print "Recipient: ") (flush)
+                 (read-line))
+   :subject  (do (print "Subject: ") (flush)
+                 (read-line))
+   :body     (do (println "Body (must be put in quotes):")
+                 (read))
+   :times    (do (print "Number of times: ") (flush)
+                 (read))})
+
+(defn send-email [details]
+  (postal/send-message {:host "smtp.mail.yahoo.com"
+                        :user (:username details)
+                        :pass (:password details)
+                        :ssl  true}
+                       {:from    (:username details)
+                        :to      (:to details)
+                        :subject (:subject details)
+                        :body    (:body details)}))
+
+(defn -main [& args]
+  (let [details (get-details)
+        times (atom (:times details))
+        threads 4
+        status (atom {:active true
+                      :done   0})]
+    (dotimes [_ threads]
+      (a/go-loop []
+        (if (and (:active @status) (pos? @times))
+          (do
+            (swap! times dec)
+            (send-email details)
+            (recur))
+          (swap! status #(update % :done inc)))))
+    (while (:active @status)
+      (if (>= (:done @status) threads)
+        (swap! status #(assoc % :active false))))))
+
+;(-main)
